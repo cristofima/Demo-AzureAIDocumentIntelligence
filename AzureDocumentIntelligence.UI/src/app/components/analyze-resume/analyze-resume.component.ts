5e16ac1e14a5d571eecf928f6dd7879c8bda6421
Cristopher Coronado <cristofima@hotmail.com>
2024-12-08 10:50:46 -0500

    refactor(UI): add success and error toasts

--- AzureDocumentIntelligence.UI/src/app/components/analyze-resume/analyze-resume.component.ts
+++ AzureDocumentIntelligence.UI/src/app/components/analyze-resume/analyze-resume.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '@/services/api.service';
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import html2pdf from 'html2pdf.js';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { FileUploadModule } from 'primeng/fileupload';
 import { lastValueFrom } from 'rxjs';
@@ -12,16 +13,18 @@ import { lastValueFrom } from 'rxjs';
   standalone: true,
   imports: [CommonModule, FileUploadModule, ButtonModule],
   templateUrl: './analyze-resume.component.html',
-  styleUrl: './analyze-resume.component.scss'
+  styleUrl: './analyze-resume.component.scss',
 })
 export class AnalyzeResumeComponent {
-
   resume?: Resume;
   isLoading = false;
   selectedFile: File | null = null;
   @ViewChild('resumeSection', { static: false }) resumeSection!: ElementRef;
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private apiService: ApiService,
+    private messageService: MessageService,
+  ) {}
 
   onFileSelected(event: any) {
     this.selectedFile = event.files[0];
@@ -37,9 +40,21 @@ export class AnalyzeResumeComponent {
     this.isLoading = true;
 
     try {
-      this.resume = await lastValueFrom(this.apiService.analyzeResume(this.selectedFile));
-    } catch (error) {
-      console.error(error);
+      this.resume = await lastValueFrom(
+        this.apiService.analyzeResume(this.selectedFile),
+      );
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Resume processed',
+      });
+    } catch {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Error processing resume',
+        sticky: true,
+      });
     } finally {
       this.isLoading = false;
     }
@@ -52,7 +67,7 @@ export class AnalyzeResumeComponent {
       margin: 1,
       filename: 'resume.pdf',
       html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+      jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
     };
 
     html2pdf().from(printContents).set(options).save();
